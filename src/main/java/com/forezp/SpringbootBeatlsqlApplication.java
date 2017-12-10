a52554ef34a917560434d081cddb1710291025a1
xiaoping2013 <364878853@qq.com>
2017-12-10 19:50:02 +0800

    project init

--- src/main/java/com/forezp/SpringbootBeatlsqlApplication.java
+++ src/main/java/com/forezp/SpringbootBeatlsqlApplication.java
@@ -31,7 +31,7 @@ public class SpringbootBeatlsqlApplication {
         SpringApplication.run(SpringbootBeatlsqlApplication.class, args);
     }
 
-    //配置文件
+    //配置文件看
 
     @Bean(initMethod = "init", name = "beetlConfig")
     public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration(){
